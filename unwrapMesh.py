28684e811aff9e71ad889b9e9d6d0d9a521523c8
JOSHUA_LOPEZ_BINDER <YOUR_EMAIL_ADDRESS>
2013-05-26 12:03:19 -0400

    seemingly correct kruskal's! displays cut edges

--- unwrapMesh.py
+++ unwrapMesh.py
@@ -45,10 +45,12 @@ def unwrapper():
 	polylineCoords,faces = getTriangleCoords(nodes)
 
 	mesh_id,mesh = generateMesh(rawNodes,faces)
-	faces,connFaces,thetaMax = getDual(mesh)
-	displayDual(faces,connFaces,thetaMax,mesh)
-	
-	displayNormals(mesh)
+	faces,edge_weights,thetaMax = getDual(mesh)
+	displayDual(faces,edge_weights,thetaMax,mesh)
+
+	foldList = getSpanningKruskal(faces,edge_weights,mesh)
+	displayCutEdges(foldList,mesh)
+	#displayNormals(mesh)
 
 	
 
@@ -63,7 +65,7 @@ def displayNormals(mesh):
 		p2 = p1 + mesh.FaceNormals.Item[i]
 		normLines.append(rs.AddLine(p1,p2))
 	createGroup("normLines",normLines)
-
+"""
 def displayDual(faces,connFaces,thetaMax,mesh):
 	#dualLines = []
 	dualRods = []
@@ -83,6 +85,26 @@ def displayDual(faces,connFaces,thetaMax,mesh):
 		dualRods.append(rs.AddCylinder(faceCenter0,faceCenter1,r))
 	#createGroup("dualLines",dualLines)
 	createGroup("dualRods",dualRods)
+"""
+
+def displayDual(faces,edge_weights,thetaMax,mesh):
+	medianEdgeLen = getMedianEdgeLen(mesh)
+	aspectRatio = 1/10.0
+	scaleFactor = medianEdgeLen*aspectRatio
+	dualRods = []
+	for tupEdge in edge_weights:
+		edgeIdx = tupEdge[0]
+		weight = tupEdge[1]
+		r = (weight/thetaMax)*scaleFactor
+		connFaces = mesh.TopologyEdges.GetConnectedFaces(edgeIdx)
+		faceCenter0 = mesh.Faces.GetFaceCenter(connFaces.GetValue(0))
+		faceCenter1 = mesh.Faces.GetFaceCenter(connFaces.GetValue(1))
+
+		dualRods.append(rs.AddCylinder(faceCenter0,faceCenter1,r))
+	createGroup("dualRods",dualRods)
+		
+
+
 
 def createGroup(groupName,objects):
 	name = rs.AddGroup(groupName)
@@ -113,13 +135,118 @@ def getMedian(edgeLens):
 	else:
 		return edgeLens[int(nEdges/2)]
 
-def getSpanningPrimAlgo(faces,connFaces):
+"""THIS SHIT DOES NOT MAKE SENSE FOR THIS!!
+def getSpanningPrimAlgo(faces,edge_weights,mesh):
 	nFaces = len(faces)
-	spanningTree = []
+	minSpanningTree = []
+	viewEdges = []
 	count = 0
 	while count<nFaces:
+		edgeIdx = None
 		if count ==0:
 			faceIdx = random.randint(0,nFaces-1)
+		print "faceIdx:%d"%faceIdx
+		#medges = mesh-edges
+		conn_medges = mesh.TopologyEdges.GetEdgesForFace(faceIdx)
+		medge_set = []
+		for i in range(conn_medges.Length):
+			medge_set.append(conn_medges.GetValue(i))
+		#print medge_set
+		medge_idxs = sorted(medge_set,key=lambda x:edge_weights[x])
+		for possibleIdx in medge_idxs:
+			if(possibleIdx not in minSpanningTree):
+				edgeIdx = possibleIdx
+				break
+		minSpanningTree.append(edgeIdx)
+		viewEdges.append(addLineForTEdge(edgeIdx,mesh))
+		connFaces = mesh.TopologyEdges.GetConnectedFaces(edgeIdx)
+
+		if(connFaces.GetValue(0)==faceIdx):
+			faceIdx = connFaces.GetValue(1)
+		else:
+			faceIdx = connFaces.GetValue(0)
+
+		count +=1
+	print str(len(viewEdges))
+	createGroup("cutEdges",viewEdges)
+"""
+
+
+
+def getSpanningKruskal(faces,edge_weights,mesh):
+	#note: have not considered open mesh, or non-manifold edges
+	#input:
+	#	faces = list of faces in mesh. necessary?
+	#	edge_weights = list of tuples elem0 = edgeIdx, elem1 = weight
+	#output:
+	#	foldList = list of edgeIdx's that are to be cut
+	treeSets = []
+	foldList = []
+	for tupEdge in edge_weights:
+		edgeIdx = tupEdge[0]
+		arrConnFaces = mesh.TopologyEdges.GetConnectedFaces(edgeIdx)
+		setConnFaces = set([arrConnFaces.GetValue(0),arrConnFaces.GetValue(1)])
+
+
+		parentSets = []
+		#print"edgeSet:"
+		#print setConnFaces
+		isLegal = True
+		for i, treeSet in enumerate(treeSets):
+			if setConnFaces.issubset(treeSet):
+				#print"--was illegal"
+				isLegal = False
+				break
+			elif not setConnFaces.isdisjoint(treeSet):
+					#print"overlapped"
+					parentSets.append(i)
+			else:
+				pass
+				#print"not subset, isdisjoint"
+
+		if isLegal==True:
+			foldList.append(edgeIdx)
+			if len(parentSets) == 0:
+				treeSets.append(setConnFaces)
+			elif len(parentSets) == 1:
+				treeSets[parentSets[0]].update(setConnFaces)
+			elif len(parentSets) == 2:
+				treeSets[parentSets[0]].update(treeSets[parentSets[1]])
+				treeSets.pop(parentSets[1])
+			elif len(parentSets)>2:
+				print"Error in m.s.t: more than two sets overlapped with edgeSet!"
+				print "len parentSets: %d\n" %len(parentSets)
+				print(treeSets)
+				print(parentSets)
+				print(setConnFaces)
+				
+
+		# wow there must be a cleaner way of doing this!!! some set tricks
+		# also the if staements could be cleaned up probs.
+	return foldList
+
+
+def displayFoldEdges(foldList,mesh):
+	foldLines = []
+	for edgeIdx in foldList:
+		foldLines.append(addLineForTEdge(edgeIdx,mesh))
+	createGroup("foldLines",foldLines)
+
+def displayCutEdges(foldLines,mesh):
+	cutLines = []
+	for i in range(mesh.TopologyEdges.Count):
+		if i not in foldLines:
+			cutLines.append(addLineForTEdge(i,mesh))
+	createGroup("cutLines",cutLines)
+
+def getKeyForMaxVal(edge_weights,medge_set):
+	pass
+
+def addLineForTEdge(edgeIdx,mesh):
+	tVerts = mesh.TopologyEdges.GetTopologyVertices(edgeIdx)
+	p1 = mesh.TopologyVertices.Item[tVerts.I]
+	p2 = mesh.TopologyVertices.Item[tVerts.J]
+	return rs.AddLine(p1,p2)
 
 
 
@@ -128,12 +255,12 @@ def getDual(mesh):
 	#	mesh
 	#ouput:
 	#	faces = list of Faces as MeshFace class (4.rhino3d.com/5/rhinocommon/)
-	#	connFaces = list of tuples (faceIdx1,faceIdx2,weight)
+	#	connFaces = list of tuples (edgeIdx,weight)
 	faces = []
 	thetaMax = -1
 	for i in range(mesh.Faces.Count):
 		faces.append(mesh.Faces.GetFace(i))
-	connFaces = []
+	edge_weights = []
 	for i in range(mesh.TopologyEdges.Count):
 		arrConnFaces = mesh.TopologyEdges.GetConnectedFaces(i)
 
@@ -142,13 +269,15 @@ def getDual(mesh):
 		angWeight = calculateAngle(arrConnFaces,mesh)
 		if angWeight > thetaMax:
 			thetaMax = angWeight
-		tupleConnFaces = (f0,f1,angWeight)
-		connFaces.append(tupleConnFaces)
-	connFaces = sorted(connFaces,key=lambda tup: tup[2])
-	
-	#pretty2DListPrint(connFaces)
+		edge_weights.append((i,angWeight))
+		#connFaces.append(tupleConnFaces)
+	edge_weights = sorted(edge_weights,key=lambda tup: tup[1],reverse=False)
+	#how to reverse order of sorting??
 
-	return faces,connFaces,thetaMax
+	
+	pretty2DListPrint(edge_weights)
+
+	return faces,edge_weights,thetaMax
 
 def pretty2DListPrint(array):
 	print "\n".join(" ".join(map(str, line)) for line in array)
