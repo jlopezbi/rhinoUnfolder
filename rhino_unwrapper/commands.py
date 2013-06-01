108e7cc57f81642c996a9baebdb53de90d5d48a4
Max Hawkins <maxhawkins@gmail.com>
2013-06-01 11:00:24 -0400

    fixup! rename some graph traversal stuff

--- rhino_unwrapper/commands.py
+++ rhino_unwrapper/commands.py
@@ -2,9 +2,11 @@ import visualization
 import layout
 import traversal
 
+# -RunPythonScript ResetEngine RhinoUnwrapper.Unwrap_cmd
+
 def unwrap(mesh):
   mesh.FaceNormals.ComputeFaceNormals()
-  meshGraph = traversal.buildMeshGraph(mesh, graph.edgeAngle)
+  meshGraph = traversal.buildMeshGraph(mesh, traversal.edgeAngle)
   foldList = traversal.getSpanningKruskal(meshGraph,mesh)
   flatEdges = layout.layoutMesh(foldList, mesh)
   visualization.drawNet(flatEdges)
