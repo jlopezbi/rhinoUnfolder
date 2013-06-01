9e786e7771f7bfbb54f698b568e00e9c9de21041
Max Hawkins <maxhawkins@gmail.com>
2013-06-01 01:44:11 -0400

    add back module reloading code

--- Unwrap_cmd.py
+++ Unwrap_cmd.py
@@ -1,6 +1,12 @@
 from rhino_unwrapper.commands import unwrap
 from rhino_unwrapper.rhino_helpers import getMesh
 
+# Reload modules each time during development
+import sys, glob, os
+modules = [os.path.basename(f)[:-3] for f in glob.glob(os.path.dirname(__file__)+"rhino_unwrapper/*.py")]
+for module in modules:
+  reload(sys.modules['rhino_unwrapper.'+module])
+
 __commandname__ = "Unwrap"
 def RunCommand( is_interactive ):
   mesh = getMesh("Select mesh to unwrap")
