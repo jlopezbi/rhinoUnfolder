f2a4163f99acd0049a483d2d3f883784738dbe4a
JOSHUA_LOPEZ_BINDER <YOUR_EMAIL_ADDRESS>
2013-06-01 10:51:36 -0400

    fixed to traversal.edgeAngle (was graph.edgeAngle)

--- rhino_unwrapper/commands.py
+++ rhino_unwrapper/commands.py
@@ -4,7 +4,7 @@ import traversal
 
 def unwrap(mesh):
   mesh.FaceNormals.ComputeFaceNormals()
-  meshGraph = traversal.buildMeshGraph(mesh, graph.edgeAngle)
+  meshGraph = traversal.buildMeshGraph(mesh, traversal.edgeAngle)
   foldList = traversal.getSpanningKruskal(meshGraph,mesh)
   flatEdges = layout.layoutMesh(foldList, mesh)
   visualization.drawNet(flatEdges)
