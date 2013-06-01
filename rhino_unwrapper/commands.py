2a7d43b455e14e847a3270108e57e1f1c637cfff
Max Hawkins <maxhawkins@gmail.com>
2013-06-01 01:44:36 -0400

    allow arbitrary meshes to be unwrapped by computing normals first

--- rhino_unwrapper/commands.py
+++ rhino_unwrapper/commands.py
@@ -3,6 +3,7 @@ import layout
 import graph
 
 def unwrap(mesh):
+  mesh.FaceNormals.ComputeFaceNormals()
   faces,edge_weights,thetaMax = graph.assignEdgeWeights(mesh)
   foldList = graph.getSpanningKruskal(faces,edge_weights,mesh)
   flatEdges = layout.layoutMesh(foldList, mesh)
