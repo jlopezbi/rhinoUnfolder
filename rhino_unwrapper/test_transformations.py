0d3067ab7d039732d3dfe6c5a4167ac07ef92ba7
JOSHUA_LOPEZ_BINDER <YOUR_EMAIL_ADDRESS>
2016-04-04 17:00:50 -0700

    test modules added

---
+++ rhino_unwrapper/test_transformations.py
@@ -0,0 +1,29 @@
+import unittest
+import Rhino
+import transformations as trans
+reload(trans)
+
+class FrameTestCase(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        o = Rhino.Geometry.Vector3d(1,1,0)
+        x =  Rhino.Geometry.Vector3d(3,0,0)
+        z = Rhino.Geometry.Vector3d(0,0,3)
+        cls.frame = trans.Frame.create_frame(o,x,z)
+
+    def test_unitized(self):
+        unit =  1.0
+        self.assertEqual(self.frame.xVec.Length,unit)
+        self.assertEqual(self.frame.yVec.Length,unit)
+        self.assertEqual(self.frame.zVec.Length,unit)
+
+    def test_show(self):
+        #check that show() method sends correct messages
+        self.frame.show()
+
+    #def test_show(self):
+        #pass
+if __name__ == "__main__":
+    suite = unittest.TestLoader().loadTestsFromTestCase(FrameTestCase)
+    unittest.TextTestRunner(verbosity=2).run(suite)
+
---
+++ rhino_unwrapper/test_unfold.py
@@ -0,0 +1,11 @@
+import unittest
+
+class UnfolderTestCase(unittest.TestCase):
+    """Tests for `unfold.py`."""
+   #shit I have to get a mesh into this thing to get it to work. uh boy
+
+    def test_some_shit(self):
+        pass
+
+if __name__ == '__main__':
+    unittest.main()
