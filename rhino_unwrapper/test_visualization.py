3527a6e25f22d340249c1471ebc0a3addd88db04
JOSHUA_LOPEZ_BINDER <YOUR_EMAIL_ADDRESS>
2016-04-08 12:09:56 -0700

    added test_visualization.py

---
+++ rhino_unwrapper/test_visualization.py
@@ -0,0 +1,29 @@
+import unittest
+import visualization as vis
+import Rhino.Geometry as geom
+reload(vis)
+
+class VisualizationTestCase(unittest.TestCase):
+
+    def test_rhino_line(self):
+        pntA = geom.Point3d(0,0,0)
+        pntB = geom.Point3d(2,2,0)
+        line = vis.rhino_line(pntA,pntB)
+        return line
+    
+    def test_show_line(self):
+        line = self.test_rhino_line()
+        vis.show_line(line,(0,255,0,0))
+
+    def test_draw_arrow(self):
+        line = self.test_rhino_line()
+        vis.draw_arrow(line,(0,255,0,255))
+
+    def test_drawVector(self):
+        pos = geom.Point3d(0,0,0)
+        vec = geom.Vector3d(2,-2,0)
+        vis.drawVector(vec,pos)
+
+if __name__ == "__main__":
+    suiteB = unittest.TestLoader().loadTestsFromTestCase(VisualizationTestCase)
+    unittest.TextTestRunner(verbosity=2).run(suiteB)
