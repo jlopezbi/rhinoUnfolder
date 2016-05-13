7b8fe1efd95ed2c158fa2f905b1fa3a58450a764
JOSHUA_LOPEZ_BINDER <YOUR_EMAIL_ADDRESS>
2016-05-13 14:50:29 -0700

    tests created for unfold

--- rhino_unwrapper/test_unfold.py
+++ rhino_unwrapper/test_unfold.py
@@ -16,9 +16,54 @@ reload(trans)
 reload(mesh)
 reload(Map)
 
-#TODO: test unfolding of full mesh, creating a net with multiple islands
+def setUpModule():
+    print("---- unfold ----")
 
-class CatsTestCase(unittest.TestCase):
+def tearDownModule():
+    print("---- module torn down ----")
+    rs.DeleteObjects(rs.AllObjects())
 
-    def test_CATS(self):
-        pass
+
+def remove_objects():
+    rs.DeleteObjects(rs.AllObjects())
+
+class UnFolderTestCase(unittest.TestCase):
+    def setUp(self):
+        self.myMesh = mesh.make_cube_mesh()
+        self.displayer = mesh.MeshDisplayer(self.myMesh)
+        self.unfolder = unfold.UnFolder(self.myMesh)
+        self.displayer.displayFacesIdx()
+        self.displayer.displayEdgesIdx()
+        
+    def test_get_arbitrary_mesh_loc_raises_error(self):
+        self.assertRaises(AssertionError,self.unfolder.get_arbitrary_mesh_loc)
+
+    def test_get_arbitrary_mesh_loc_gets_valid(self):
+        cuts = [3,4,10,7,2]
+        self.myMesh.set_cuts(cuts)
+        #self.displayer.display_edges(cuts)
+        meshLoc = self.unfolder.get_arbitrary_mesh_loc()
+        self.assertIn(meshLoc.face, self.myMesh.face_indices())
+        self.assertIn(meshLoc.edge, self.myMesh.get_cuts())
+        self.assertIn(meshLoc.edge, self.myMesh.getFaceEdges(meshLoc.face))
+
+    def test_get_arbitrary_mesh_loc_needs_correct_cuts(self):
+        self.myMesh.set_cuts([11])
+        def getter(canditate_faces):
+            return 0
+        self.assertRaises(AssertionError,self.unfolder.get_arbitrary_mesh_loc,getter)
+
+    def test_unfolds_all_segments(self):
+        cuts = [4,3,10,11,6,5,8,0]
+        self.myMesh.set_cuts(cuts)
+        self.displayer.display_edges(cuts)
+        self.unfolder.unfold()
+        self.unfolder.net.display()
+
+
+if __name__=='__main__':
+    loader = unittest.TestLoader()
+    suite = unittest.TestSuite()
+    suite.addTest(loader.loadTestsFromTestCase(UnFolderTestCase))
+    unittest.TextTestRunner(verbosity=2).run(suite)
+
