b33c232a65af31cf163a0cec1aaf7e3b1bcbadc2
JOSHUA_LOPEZ_BINDER <YOUR_EMAIL_ADDRESS>
2016-05-18 11:06:03 -0700

    testing get_island_for_line

--- rhino_unwrapper/test_net.py
+++ rhino_unwrapper/test_net.py
@@ -92,20 +92,30 @@ class IslandTestCase(unittest.TestCase):
         frame.show()
         correct_frame = trans.Frame.create_frame_from_tuples((5,5,0),(0,-1,0),(1,0,0))
         self.assertTrue(correct_frame.is_equal(frame))
-        
+
+class StubbedIsland(object):
+    def __init__(self):
+        self.group_name = rs.AddGroup()
+        self.line_guid = rs.AddLine((0,0,0),(5,5,0))
+        rs.AddObjectToGroup(self.line_guid,self.group_name)
+
+class NetTestCase(unittest.TestCase):
+    def setUp(self):
+        self.net = Net.Net()
+        self.net.add_island(StubbedIsland())
+
+    def test_get_island_for_line(self):
+        islands = self.net.islands.values()
+        correct_island = islands[0]
+        line_guid = correct_island.line_guid
+        island = self.net.get_island_for_line(line_guid)
+        self.assertEqual(island,correct_island)
+
 
 
 if __name__ == "__main__":
-    suite = unittest.TestLoader().loadTestsFromTestCase(IslandTestCase)
+    loader = unittest.TestLoader()
+    suite = unittest.TestSuite()
+    suite.addTest(loader.loadTestsFromTestCase(IslandTestCase))
+    suite.addTest(loader.loadTestsFromTestCase(NetTestCase))
     unittest.TextTestRunner(verbosity=2).run(suite)
-    
-#    def test_tack_on_facet(self):
-#        self.test_add_first_face_from_verts()
-#        Points = []
-#        Points.append(geom.Point3d(10.0,0.0,0.0))
-#        Points.append(geom.Point3d(10.0,5.0,0.0))
-#        face,edges = self.island.tack_on_facet(edge=1,points=Points)
-#        self.assertEqual(edges,[4,5,6])
-#        self.assertEqual(face,1)
-#        self.island.draw_edges()
-#        self.island.draw_faces()
