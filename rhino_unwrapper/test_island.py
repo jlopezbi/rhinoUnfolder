a6871e40d6e74768d886b0ff5cd8f95165704aee
JOSHUA_LOPEZ_BINDER <YOUR_EMAIL_ADDRESS>
2016-05-24 08:51:26 -0700

    more modular tests, IslandAvoidTestCase

--- rhino_unwrapper/test_island.py
+++ rhino_unwrapper/test_island.py
@@ -16,21 +16,29 @@ def tearDownModule():
 def remove_objects():
     rs.DeleteObjects(rs.AllObjects())
 
+def make_five_by_five_square_island(island):
+    island.add_vert_from_points(0.0,0.0,0.0) #0
+    island.add_vert_from_points(5.0,0.0,0.0) #1
+    island.add_vert_from_points(0.0,5.0,0.0) #2
+    island.add_vert_from_points(5.0,5.0,0.0) #3
+    face = island.add_first_face_from_verts(0,1,3,2)
+    for edge in range(len(island.flatEdges)): island.change_to_cut_edge(edge)
+    return face
+
 class IslandTestCase(unittest.TestCase):
+
+    @classmethod
+    def tearDownClass(cls):
+        remove_objects()
 
     def setUp(self):
         self.island = island.Island()
-
-    def make_five_by_five_square_island(self):
-        self.island.add_vert_from_points(0.0,0.0,0.0) #0
-        self.island.add_vert_from_points(5.0,0.0,0.0) #1
-        self.island.add_vert_from_points(0.0,5.0,0.0) #2
-        self.island.add_vert_from_points(5.0,5.0,0.0) #3
-        face = self.island.add_first_face_from_verts(0,1,3,2)
-        return face
+    
+    def tearDown(self):
+        self.island.clear()
 
     def test_add_first_face_from_verts(self):
-        face = self.make_five_by_five_square_island()
+        face = make_five_by_five_square_island(self.island)
         self.assertEqual(self.island.flatFaces[face].edges,[0,1,2,3])
         self.island.draw_edges()
         self.island.draw_faces()
@@ -64,7 +72,7 @@ class IslandTestCase(unittest.TestCase):
         self.assertEqual(self.island.flatEdges[0].toFace,1)
 
     def _test_add_face_from_edge_and_new_verts(self):
-        self.make_five_by_five_square_island()
+        make_five_by_five_square_island()
         new_vert =  self.island.add_vert_from_points(10.0,0.0,0.0)
         edge = self.island.flatEdges[1]
         self.island.add_face_from_edge_and_new_verts(edge,[new_vert])
@@ -78,7 +86,7 @@ class IslandTestCase(unittest.TestCase):
         self.island.draw_verts()
 
     def test_transform(self):
-        self.make_five_by_five_square_island()
+        make_five_by_five_square_island(self.island)
         self.island.draw_edges()
         self.island.clear()
         vec = geom.Vector3d(20.0,0.0,0.0) 
@@ -86,33 +94,76 @@ class IslandTestCase(unittest.TestCase):
         self.island.draw_edges()
 
     def test_get_frame_reverse_edge(self):
-        firstFace = self.make_five_by_five_square_island()
+        firstFace = make_five_by_five_square_island(self.island)
         frame = self.island.get_frame_reverse_edge(edge=1,face=firstFace)
         frame.show()
         correct_frame = trans.Frame.create_frame_from_tuples((5,5,0),(0,-1,0),(1,0,0))
         self.assertTrue(correct_frame.is_equal(frame))
 
+
+class IslandAvoidTestCase(unittest.TestCase):
+
+    def setUp(self):
+        self.island = island.Island()
+
+    def make_overlapping_islands(self):
+        islandA = self.island
+        islandB = island.Island()
+        make_five_by_five_square_island(islandA)
+        make_five_by_five_square_island(islandB)
+        islandB.translate(geom.Vector3d(2,0,0))
+        islandA.draw_edges()
+        islandB.draw_edges()
+        return islandA,islandB
+
     def test_get_boundary_polyline(self):
-        self.make_five_by_five_square_island()
-        for edge in range(len(self.island.flatEdges)): self.island.change_to_cut_edge(edge)
+        make_five_by_five_square_island(self.island)
         self.island.clear()
         self.island.draw_edges()
         boundary = self.island.get_boundary_polyline()
         self.assertTrue(rs.IsPolyCurve(boundary))
         self.assertTrue(rs.IsCurveClosed(boundary))
         self.assertTrue(rs.IsCurvePlanar(boundary))
+        self.island.clear()
 
-    def test_get_bounding_box(self):
-        pass
+    def test_is_overlapping(self):
+        island_a,island_b = self.make_overlapping_islands()
+        self.assertTrue(island_a.is_overlapping(island_b))
+        island_b.translate(geom.Vector3d(10,0,0))
+        island_b.clear()
+        island_b.draw_edges()
+        self.assertFalse(island_a.is_overlapping(island_b))
+        self.assertFalse(island_b.is_overlapping(island_a))
+        island_c = island.Island()
+        island_c.add_vert_from_points(1.0,1.0,0.0) #0
+        island_c.add_vert_from_points(4.0,1.0,0.0) #1
+        island_c.add_vert_from_points(4.0,4.0,0.0) #2
+        island_c.add_vert_from_points(1.0,4.0,0.0) #3
+        face = island_c.add_first_face_from_verts(0,1,3,2)
+        for edge in range(len(island_c.flatEdges)): island_c.change_to_cut_edge(edge)
+        island_c.draw_edges()
+        self.assertTrue(island_c.is_overlapping(island_a))
+        self.assertTrue(island_a.is_overlapping(island_c))
+
+        island_a.clear()
+        island_b.clear()
+        island_c.clear()
+
+    def test_get_bounding_rectangle(self):
+        make_five_by_five_square_island(self.island)
+        self.island.draw_edges()
+        rect = self.island.get_bounding_rectangle()
+        self.island.clear()
 
     def test_avoid_other_island(self):
-        #NOTE: working here; a function that moves the island so that it is 
-        #not overlapping the given island
-        #self.island.avoid_other_island(someOtherIsland)
-        pass
+        islandA,islandB = self.make_overlapping_islands()
+        self.assertTrue(islandA.is_overlapping(islandB))
+        islandA.avoid_other(islandB)
+        self.assertFalse(islandA.is_overlapping(islandB))
 
 if __name__ == "__main__":
     loader = unittest.TestLoader()
     suite = unittest.TestSuite()
     suite.addTest(loader.loadTestsFromTestCase(IslandTestCase))
+    suite.addTest(loader.loadTestsFromTestCase(IslandAvoidTestCase))
     unittest.TextTestRunner(verbosity=2).run(suite)
