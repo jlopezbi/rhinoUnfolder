ccd596185dd40cc3d319a8b4c351c7a35f25cec2
JOSHUA_LOPEZ_BINDER <YOUR_EMAIL_ADDRESS>
2016-05-24 09:32:12 -0700

    using net public interface for getting islands

--- rhino_unwrapper/distribute.py
+++ rhino_unwrapper/distribute.py
@@ -1,7 +1,7 @@
 ''' Distributes islands in a net so that they can be edited and looked at easily'''
 
 def spread_out_islands_horizontally(net):
-    islands = net.islands
+    islands = net.get_island_list()
     for i,island in enumerate(islands):
         if i!=0:
             island.move_to_edge(islands[i-1])
