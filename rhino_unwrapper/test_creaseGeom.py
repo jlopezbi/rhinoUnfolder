aa1fd3f90167bd90759d4863a1ee0414cfa969e3
JOSHUA_LOPEZ_BINDER <YOUR_EMAIL_ADDRESS>
2016-06-07 16:52:31 -0700

    working tests for slot_crease

--- rhino_unwrapper/test_creaseGeom.py
+++ rhino_unwrapper/test_creaseGeom.py
@@ -5,11 +5,22 @@ reload(creaseGeom)
 
 class SlotCreaseTestCase(unittest.TestCase):
 
-    def test_get_arc_cap_works(self):
-        pntA,pntB,pntC = creaseGeom.get_arc_cap((0.0,0.0,0.0,), (5.0,0.0,0.0),2,1)
-        #TODO: add assertions for point locations
-        rs.AddArc3Pt(pntA,pntB,pntC)
+    def test_slot_crease(self):
+        edge_length = 10.0
+        pntI = (0.0,0.0,0.0)
+        pntJ = (edge_length,0.0,0.0)
+        offset = 2.0
+        width = 1.0
+        creaseGeom.slot_crease(pntI,pntJ,offset,width)
 
+    def test_get_arc_cap_works(self):
+        offset = 2
+        radius = 1
+        pntA,pntC,pntB = creaseGeom.get_arc_cap((0.0,0.0,0.0,), (5.0,0.0,0.0),offset,radius)
+        self.assertTrue(rs.PointCompare(pntB,(offset,0.0,0.0)) )
+        self.assertTrue(rs.PointCompare(pntA,(offset+radius,-radius,0.0)) )
+        self.assertTrue(rs.PointCompare(pntC,(offset+radius,radius,0.0)) )
+        rs.AddArc3Pt(pntA,pntC,pntB)
 if __name__ == "__main__":
     loader = unittest.TestLoader()
     suite = unittest.TestSuite()
