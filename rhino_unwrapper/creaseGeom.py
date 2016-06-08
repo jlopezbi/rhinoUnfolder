1de4d312e5befc21fc17f3f2685d1b4986e17ec7
JOSHUA_LOPEZ_BINDER <YOUR_EMAIL_ADDRESS>
2016-06-08 10:47:57 -0700

    widht/radius bugfix, color

--- rhino_unwrapper/creaseGeom.py
+++ rhino_unwrapper/creaseGeom.py
@@ -2,7 +2,7 @@ import rhinoscriptsyntax as rs
 import rhino_helpers
 reload(rhino_helpers)
 
-def pill_shape(pntI,pntJ,offset,width):
+def pill_shape(pntI,pntJ,offset,width,color=(0,0,0)):
     '''
     creates a pill shape between the two points
     returns the polycurve guid
@@ -12,15 +12,18 @@ def pill_shape(pntI,pntJ,offset,width):
            \        /
              A -- F
     '''
-    pntA,pntC,pntB = get_arc_cap(pntI,pntJ,offset,width)
-    pntD,pntF,pntE = get_arc_cap(pntJ,pntI,offset,width)
+    radius = width/2.0
+    pntA,pntC,pntB = get_arc_cap(pntI,pntJ,offset,radius)
+    pntD,pntF,pntE = get_arc_cap(pntJ,pntI,offset,radius)
     first_arc = rs.AddArc3Pt(pntA,pntC,pntB)
     second_arc = rs.AddArc3Pt(pntD,pntF,pntE)    
     first_line = rs.AddLine(pntC,pntD)
     second_line = rs.AddLine(pntF,pntA)
     geom = [first_arc,second_arc,first_line,second_line]    
     curves = rs.JoinCurves(geom,delete_input=True)
+    assert len(curves) == 1, "in pill_shape JoinCurves failed"
     curve = curves[0]
+    rs.ObjectColor(curve,color)
     return curve
 
 def get_arc_cap(pntI,pntJ,offset,radius):
